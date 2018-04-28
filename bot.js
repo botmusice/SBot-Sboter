72776b44e777cb16c8a7b5273dfdd0246149e14f
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-28 13:55:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,28 +7,6 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("-info")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setAuthor(client.user.username,client.user.avatarURL)
-            .setThumbnail(client.user.avatarURL)
-            .setColor('RANDOM')
-            .setTitle('``INFO SPEED BOT`` ')
-            .addField('``Uptime``', [timeCon(process.uptime())], true)
-            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
-            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
-            .addField('``servers``', [client.guilds.size], true)
-            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
-            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
-            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
-            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
-                  .addField('``My Prefix``' , `[ - ]` , true)
-                  .addField('``My Language``' , `[ Java Script ]` , true)
-                  .setFooter('By | 𝒮ˣᶜєℓмυsαυɪ_ɢĸ#7300/SpeedMC#4421 ')
-    })
-}
-});
 
 client.on('message', msg => {
 	var prefix = "-";
