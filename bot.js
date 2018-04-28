a17d3d0e7483590f32894604e77ffe2541a5f581
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-28 20:20:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,24 +6,16 @@ const UserBlocked = new Set();
 
 client.on('ready', () => {
 	console.log('I am ready!'); 
-
-client.on('ready', function(){
-    var ms = 40000 ;
-    var setGame = ['-inv','-help',`On ${client.guilds.size} Servers`, `${client.users.size} Users`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);
-    }, ms);
+	
+client.on('message', message => {
+    if (message.content === '-help')
+   var embed = new Discord.RichEmbed()
+        .setTitle('تم ارسال جميع الاوامر على الخاص ,, :e_mail: ')
+        .setColor('RED')
+       message.channel.sendEmbed(embed)
 
 });
+
 
 client.on("message", message => {
 	var prefix ="-";
