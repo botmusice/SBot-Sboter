98134ec3a3957cb729d4e3c0310026dcec8e90fc
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-04-30 22:55:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,16 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 client.on('message', message => {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if (message.content.startsWith('*bc-all')){
+    if(!message.author.id === '349616310734553088') return;
+    message.channel.sendMessage('جاري ارسال الرسالة :white_check_mark:')
+    client.users.forEach(m =>{
+    m.sendMessage(args)
+    })
+    }
+    });
+client.on('message', message => {
 	var prefix ="-";
 if (message.content.startsWith(prefix + 'help')) {
   var embed = new Discord.RichEmbed() 
