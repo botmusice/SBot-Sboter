9aebedd71cdf8ed40bc750219977f115cb0a6dcf
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-01 11:45:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Music = require('discord.js-musicbot-addon');
 const client = new Discord.Client(); 
-const fs = require("fs");
+const fs = require("fs"); 
 const UserBlocked = new Set();
 const music = new Music(client, {
     prefix: "-", 
