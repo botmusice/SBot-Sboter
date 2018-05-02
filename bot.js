087405893597bccbe78e6e0e72cd871e7909955b
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-02 18:00:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,6 +72,26 @@ let PREFIX = '-'
 });
 
 client.on('message', message => {
+            var prefix = "-";
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+
+    let args = message.content.split(" ").slice(1);
+
+     if (command == "say") {
+        if (!message.channel.guild) return message.reply('** This command only for servers **');
+      let Dabooka = new Discord.RichEmbed()
+            .setDescription(args.join("  "))
+        message.channel.send(Dabooka)
+        message.delete();
+    }
+
+});
+
+client.on('message', message => {
 	var prefix ="-";
 if (message.content.startsWith(prefix + 'help')) {
   var embed = new Discord.RichEmbed() 
