e4bc212d27e3f00357f7fdb0833282e3283c21de
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 23:49:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,31 @@ client.on('ready', () => {
   });
 
 
+client.on('message', msg => {
+	var prefix = "-";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clr") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
 var prefix = ".";
 var aoasm =[
     {q:"ما عاصمة **افغانستان**",a:"كبل"},
