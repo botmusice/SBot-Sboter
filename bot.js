3490471aa1e78013cb0019c0ce5bd6b2c4d66b43
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 17:44:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,84 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 
+
+var prefix = ".";
+var aoasm =[
+    {q:"ما عاصمة **افغانستان**",a:"كبل"},
+
+    
+    
+    
+   ];
+   client.on("message", async message => {
+	   var prefix = ".";
+    if(message.content == prefix+"عواصم"){
+
+        UserBlocked.add(message.guild.id)
+        var ask = aoasm[Math.floor(Math.random() * aoasm.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('عاصمة')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.q);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "عاصمة") return
+           if(result.content == ask.a){
+
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+
+                                  var embedx = new Discord.RichEmbed()
+                .setTitle(':x:خطاء')
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setColor("RANDOM")
+                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
+
+var prefix = ".";
+var fkk =[
+        {f:" بسم الله الرحمن الرحيم",k:"ب س م ا ل ل ه ا ل ر ح م ن ا ل ر ح ي م"},
+
+   ];
+
+
+
+   client.on('message', message => {
+       var prefix = ".";
+    if(message.content == prefix + "فكك") {
+        var ask = fkk[Math.floor(Math.random() * fkk.length)];
+message.channel.send(`${ask.f}`)
+.then(() => {
+  message.channel.awaitMessages(response => response.content === `${ask.k}`, {
+    max: 1,
+    time: 20000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      
+message.channel.send(`لقد قمت بكتابة الإجابة الصحيحة خلال الفتره المحددة (:`)
+  })
+    .catch(() => {
+      message.channel.send('****');
+    })
+})
+
+    }
+   });
+
 var coinflip =["https://i.imgur.com/ounEiyN.png", "https://i.imgur.com/xUwvSde.png"]
      client.on('message', message => {    
 	     var prefix = "-";
