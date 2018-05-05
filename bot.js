23ea141f2d1d627a07c99c2d0e90d07480f1f076
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-06 00:17:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,41 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 
+client.on('message', message => {
+	var prefix = "-";
+if (message.author.id === client.user.id) return;
+if (message.guild) {
+let embed = new Discord.RichEmbed()
+let copy = "Elmusaui_GK"
+let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'broadcast') {
+if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+if (!args[1]) {
+return;
+}
+  message.guild.members.forEach(m => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return;
+      var bc = new Discord.RichEmbed()
+  .setTitle('Broadcast')       
+.addField('Server', message.guild.name)    
+.addField('Sender', message.author.username)    
+.addField('Message', args)    
+ .setFooter(copy, client.user.avatarURL);
+      .setThumbnail(message.guild.iconURL)
+      .setColor('RANDOM')
+      m.sendMessage(args)
+  });
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply(":x: **ليس لديك صلاحية للنشر هنا**");
+  const AziRo = new Discord.RichEmbed()   
+  .setColor('RANDOM')
+  message.channel.sendEmbed(AziRo);          
+}
+} else {
+  return;
+}
+});
+
+
 
 client.on('message', msg => {
 	var prefix = "-";
