6c43cbf40f9db38bfe5f827d0dbcfef60e1c8fdd
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 17:23:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1316,8 +1316,6 @@ embed.addField(":cloud_tornado:  الاسم", `**[ ${msg.author.username}#${msg.
 .addField(":robot:  هل هو بوت", `**[ ${msg.author.bot.toString().toUpperCase()} ]**`, true)
  .addField(': **[دخولك لديسكورد قبل]**', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
 .addField(': **[انضمامك لسيرفر قبل]**', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true);              
- .setFooter(`SpeedBot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
-    .setThumbnail(heg.avatarURL);
 msg.channel.send({embed: embed})
 }
 });
