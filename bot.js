a6f7e9727cfc6febde4165a83a9d7342c84d64d2
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 23:55:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,28 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 
+client.on('message', message => {
+	var prefix = "-";
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
 
 client.on('message', msg => {
 	var prefix = "-";
