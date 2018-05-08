ed1904d7dc0712a14df5728c739d043028e40960
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-08 21:59:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,23 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "+roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});
+
 client.on('guildMemberAdd', member => {
 
     const channel = member.guild.channels.find('name', 'welcome');
