02a1ce61a09ea06b2b3a7c79a96232ab9c0325e7
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-08 22:15:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,27 +31,7 @@ client.on('message', message =>{
     }
 });
 
-client.on('guildMemberAdd', member => {
 
-    const channel = member.guild.channels.find('name', 'welcome');
-  
-    const millis = new Date().getTime() - member.user.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-
-
-
-
-  
-    const embed = new Discord.RichEmbed()
-    
-    .setColor("#1f0707")
-    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
-    .setAuthor(member.user.tag, member.user.avatarURL);
-    channel.sendEmbed(embed);
-
-  
-});
 
 client.on('message', async message =>{
   var prefix = "-";
