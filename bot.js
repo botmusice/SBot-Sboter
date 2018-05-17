f0174185b3af7980c78cda47f1b2cbfb698304f1
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-17 15:42:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,41 @@ const sql = require("sqlite");
 ,ti={} 
 ,spee={};
 
+client.on('message',function(message) {
+                  if(!message.channel.guild) return;
+
+  const prefix = "-";
+                    if (message.content === prefix + "discrim") {
+    let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1);
+    
+    if (message.author.bot) return;
+    
+    var discri = args[0]
+    let discrim
+    if(discri){
+    discrim = discri;
+    }else{
+    discrim = message.author.discriminator;
+    }
+    if(discrim.length == 1){
+        discrim = "000"+discrim
+    }
+    if(discrim.length == 2){
+        discrim = "00"+discrim
+    }
+    if(discrim.length == 3){
+        discrim = "0"+discrim
+    }
+
+        const users = client.users.filter(user => user.discriminator === discrim).map(user => user.username);
+        return message.channel.send(`
+            **Found ${users.length} users with the discriminator #${discrim}**
+            ${users.join('\n')}
+        `);
+}
+});
+
 client.on('message', message => {
 	var prefix = "-";
         if (message.author.id === client.user.id) return;
