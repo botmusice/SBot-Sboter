391274d242c1369156b32c3f96274b7af78b12b3
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-17 12:39:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,6 +46,24 @@ message.channel.send(image)
     }
 });
 
+client.on('ready', function(){	
+    var ms = 40000 ;	
+    var setGame = ['-help','رمضان كريم للجميع'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
+});
+
  client.on('message',( message )=>{
 var attentions = {};
 var prefix = '-';
