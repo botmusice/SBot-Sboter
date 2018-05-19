94ebd47daa3649ca2494b7258e0d67ef7c6403e7
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-19 00:28:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,31 @@ const sql = require("sqlite");
  const pretty = require('pretty-ms')
 ,ti={} 
 ,spee={};
+const Music = require('discord.js-musicbot-addon');
+const music = new Music(client, {
+    prefix: ".", // Prefix for the commands.
+    youtubeKey: 'AIzaSyApvbcgvYRGulf1I1Ffjfhr2K-S6TX0e9w',
+    global: false,            // Non-server-specific queues.
+    maxQueueSize: 50,        // Maximum queue size of 25.
+    playCmd: 'play',        // Sets the name for the 'play' command.
+    playAlts: ["play", 'paly'],
+    volumeCmd: 'vol',     // Sets the name for the 'volume' command.
+    thumbnailType: 'high',
+    leaveCmd: 'stop',      // Sets the name for the 'leave' command.
+    anyoneCanSkip: true,
+    disableLoop: false,
+    searchCmd: 'search',
+    requesterName: true,
+    inlineEmbeds: true,     
+    queueCmd: 'queue',
+    queueAlts: ['queue', 'queueue'],
+    pauseCmd: 'pause',
+    resumeCmd: 'resume',
+    skipCmd: 'skip',
+    skipAlts: ["skip", "skipp"],
+    loopCmd: 'loop',
+    enableQueueStat: true,
+  });
 
 
 client.on('message', message => {
