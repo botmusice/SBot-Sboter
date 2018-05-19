1810ab1f6ab4fae3f40a272acbc30cbe76ce0765
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-19 14:47:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const moment = require("moment");
 const { Client, Util } = require('discord.js'); 
 const UserBlocked = new Set();  
 const jimp = require('jimp'); 
+const Canvas = require('canvas')
 const math = require('math-expression-evaluator');
 const stripIndents = require('common-tags').stripIndents;
 const figlet = require('figlet');
@@ -50,6 +51,44 @@ const music = new Music(client, {
     enableQueueStat: true,
   });
 
+client.on('guildMemberAdd', member => {
+      
+        if (member.guild.id === "377989934897954816") {
+        var w = ['https://cdn.discordapp.com/attachments/447077848772247552/447397991905755146/images.jpg'];
+           let Image = Canvas.Image,
+               canvas = new Canvas(401, 202),
+               ctx = canvas.getContext('2d');
+           ctx.patternQuality = 'bilinear';
+           ctx.filter = 'bilinear';
+           ctx.antialias = 'subpixel';
+           ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+           ctx.shadowOffsetY = 2;
+           ctx.shadowBlur = 2;
+           fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+               if (err) return console.log(err);
+               let BG = Canvas.Image;
+               let ground = new Image;
+               ground.src = Background;
+               ctx.drawImage(ground, 0, 0, 401, 202);
+   
+   })
+                   let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
+                   jimp.read(url, (err, ava) => {
+                       if (err) return console.log(err);
+                       ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                           if (err) return console.log(err);
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(75, 100, 63, 0, Math.PI*2);
+                                 ctx.closePath();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 10, 25, 135, 170);                    
+   client.channels.get("ايدي الروم").sendFile(canvas.toBuffer())
+  client.channels.get("ايدي الروم").send(`Welcome ${member} To __${member.guild.name}__`):nox: 
+   })})  }
+   });
 
 client.on('message', message => {
 	var prefix ="-";
@@ -1672,7 +1711,6 @@ client.on('message', message => {
 
 
 if(command == "draw") {
-    var Canvas = require('canvas')
   , Image = new Canvas.Image
   , canvas = new Canvas(450, 170)
   , ctx = canvas.getContext('2d');
