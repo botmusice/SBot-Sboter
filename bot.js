d3826434cb9a82d7d7080e451657f54a432c3f86
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-21 15:24:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client(); 
+const client = new Discord.Client();  
 const giphy = require('giphy-api')();
 const googl = require('goo.gl');
 const translate = require('google-translate-api'); 
