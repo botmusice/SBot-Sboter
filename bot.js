1381504527692c30d47f6da57cdfcc0c21c9afa0
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-23 15:56:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,6 +52,28 @@ const music = new Music(client, {
     enableQueueStat: true,
   });
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("SERVER")
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+
 client.on('message', message => {
 	var prefix = "-";
 if(!message.channel.guild) return;
