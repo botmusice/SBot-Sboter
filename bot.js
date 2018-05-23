6729f5fda55e79d204f04625df7a9543595907ca
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-23 01:09:13 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,55 +54,6 @@ const music = new Music(client, {
 
 client.on('message', message => {
 	var prefix = "-";
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'broadcast')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let BcList = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله ${args}`)
-.setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست\nيمكنك اضافة اسم السيرفر في البرودكاست عن طريق كتابة <server>\nيمكنك اضافة اسم المرسل في البرودكاست عن طريق كتاية <by>\nيمكنك منشنة العضو في الرساله عن طريق كتابة <user>`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
-msg.react('📝')
-.then(() => msg.react('✏'))
-.then(() =>msg.react('📝'))
- 
-let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
-let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
- 
-let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
-let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
- 
- 
-EmbedBc.on("collect", r => {
- 
-message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-let EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setDescription(EmbedRep)
-.setThumbnail(message.author.avatarURL)
-m.send({ embed: bc })
-msg.delete();
-})
-})
-NormalBc.on("collect", r => {
-  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-let NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-m.send(NormalRep);
-msg.delete();
-})
-})
-})
-}
-});
-
-client.on('message', message => {
-	var prefix = "-";
 if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'move')) {
  if (message.member.hasPermission("MOVE_MEMBERS")) {
@@ -1511,59 +1462,6 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`بدء تشغيل: **${song.title}**`);
 }
-
-
-client.on('message', message => {
-	var prefix = "-";
-    let args = message.content.split(' ').slice(1)
-    if (message.author.id != 349616310734553088) return;
-    if (message.content.startsWith(prefix + 'setGame')) {
-        if (message.author.id != 349616310734553088) return;
-        else {
-            client.user.setGame(args.join(' '));
-            message.channel.send(`My New Game is = ${args.join('  ')}`)
-
-        }
-    }
-    if (message.author.id != 349616310734553088) return;
-    if (message.content.startsWith(prefix + 'setListening')) {
-        if (message.author.id != 349616310734553088) return;
-        else {
-            client.user.setActivity(args.join(' '), {
-                type: 'LISTENING'
-            });
-            message.channel.send(`My New Listening is = ${args.join('  ')}`)
-
-        }
-    }
-    if (message.author.id != 349616310734553088) return;
-    if (message.content.startsWith(prefix + 'setWatching')) {
-        if (message.author.id != 349616310734553088) return;
-        else {
-            client.user.setActivity(args.join(' '), {
-                type: 'WATCHING'
-            });
-            message.channel.send(`My New Watching is = ${args.join('  ')}`)
-
-        }
-    }
-    if (message.author.id != 349616310734553088) return;
-    if (message.content.startsWith(prefix + 'setName')) {
-        if (message.author.id != 349616310734553088) return;
-        else {
-            client.user.setUsername(args.join(' '));
-            message.channel.send(`My New Name is = ${args.join('  ')}`)
-        }
-    }
-    if (message.author.id != 349616310734553088) return;
-    if (message.content.startsWith(prefix + 'setStatus')) {
-        if (message.author.id != 349616310734553088) return;
-        else {
-            client.user.setStatus(args.join(' '));
-            message.channel.send(`My New Status is = ${args.join('  ')}`)
-        }
-    }
-});
 
 client.on('guildMemberAdd', member => {
     if (!member || !member.id || !member.guild) return;
