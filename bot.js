bd779ea745be5d71c9d26fed9bfb9437a856dbf3
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-27 20:19:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,27 +51,11 @@ const music = new Music(client, {
     enableQueueStat: true,
   }); 
 
-client.on('message', message => {
-	var prefix = "-";
-if(message.content.startsWith(prefix + '8ball')) {
-if(!message[2]) return message.reply ("Please ask a full question");
-let replies = ["Yes.","No.","I dont know.","Ask again later"];
-
-let result = Math.floor ((Math.random () * replies.length));
-let question = args.slice(1).join(" ");
-
-let ballembed = new Discord.RichEmbed()
-.setAuthor(message.author.tag)
-.setColor("RANDOM")
-.addField("Question", question)
-.addField("Answer", replies[result])
-
-message.channel.send(ballembed);
-
-
-
-}
-})
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('431873570159722503').roles.find('name', 'rainbow').edit({color: 'RANDOM'}) 
+    },100);
+});
 
 client.on('message', message => {
 	var prefix ="-";
