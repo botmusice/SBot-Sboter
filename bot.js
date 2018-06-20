86b13a10666775ecf1319161d87b38e449280341
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-06-20 19:17:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,19 +27,6 @@ const sql = require("sqlite");
 ,spee={};
 
 client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('>-bcall')){
- if (message.author.id !== '349616310734553088') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '349616310734553088') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
-
-client.on('message', message => {
     if (message.content.startsWith("-info")) {
     message.channel.send({
         embed: new Discord.RichEmbed()
