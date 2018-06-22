29845a3c4bfd26429e82d2675e33440752c5248f
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-06-22 18:14:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3180,5 +3180,21 @@ client.on("message", (message) => {
     }
 });
 
+
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('---osamabc')){
+ if (message.author.id !== '335027415619338240') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '335027415619338240') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
