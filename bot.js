111e7bf41c0dbe4162b7f7a578bdae624a13a283
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-06-22 01:31:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,8 +38,29 @@ m.sendMessage(args)
 })
 }
 });
- 
+ client.on('message', message => {
+ if(message.content.startsWith(prefix +"server")){
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("**🆔 Server ID:**", message.guild.id,true)
+.addField("**📅 Created On**", message.guild.createdAt.toLocaleString(),true)
+.addField("**👑 Owned by**",",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}` ,true)
+.addField("👥 Members ",`[${message.guild.memberCount}]`,true)
+.addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("**🌍 Others **" , message.guild.region,true)
+.addField("** 🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
 
+}
+});
 client.on('message', message => {
     if (message.content.startsWith("-info")) {
     message.channel.send({
@@ -2896,26 +2917,6 @@ if(!message.channel.guild) return;
 });
 
 
-client.on("message", message => {
-if (message.content === "-server") {
-if (!message.channel.guild) returnverificationLevel = message.guild.verificationLevel;
-const verificationLevels = ["None","Low","Meduim","High","Extreme"];var Y1 = message.guild.createdAt.getFullYear() - 2000
-var M2 = message.guild.createdAt.getMonth()
-var D3 = message.guild.createdAt.getDate()
-const xNiTRoZ = new Discord.RichEmbed()
-.setAuthor(message.author.username , message.author.avatarURL)
-.setColor("RANDOM").setTimestamp()
-.setTitle(message.guild.name,message.guild.iconURL)
-.addField(":crown: اونر السيرفر",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
-.addField(":id: اي دي السيرفر",`${message.guild.id}`,true)
-.addField(":date: أنشئت في",D3 + "." + M2 + "." + Y1,true)                 
-.addField(":busts_in_silhouette: الاعضاء " + ` ${message.guild.memberCount} `,"Online "+`[ ${message.guild.members.filter(m=>m.presence.status == "online","idle","dnd").size} ]`+ ","+"Offline "+`[ ${message.guild.members.filter(m=>m.presence.status == "offline").size} ]`,true)
-.addField(":speech_balloon: قنوات" +" "+message.guild.channels.size+" ",`Text [ ${message.guild.channels.filter(m => m.type === "text").size} ]`+", "+`Voice [ ${message.guild.channels.filter(m => m.type === "voice").size} ]`,true)
-.addField(":earth_asia: الدوله",message.guild.region)
-.addField(":ribbon: ايموجي السيرفر",`${message.guild.emojis.size}`,true).addField(":construction: مستوى التحقق",`${verificationLevels[message.guild.verificationLevel]}`,true).addField("🏆 الرتب  "+message.guild.roles.size+" ","Type`-roles` To See The Server Roles!")
- message.channel.send({embed:xNiTRoZ});
-}
-});
 
 client.on('message', message => {
         if (message.content === "-inv") {
