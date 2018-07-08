2e0d54e6733db559d507bd58193cd15548a7e544
jgvgvjk <40689329+jgvgvjk@users.noreply.github.com>
2018-07-08 18:16:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2156,7 +2156,7 @@ client.on('message', message => {
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
         .setTitle(`:small_orange_diamond: اضغط هنا `)
-        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=465263100543631370&permissions=8&scope=bot`)
+        .setURL(`https://discordapp.com/api/oauth2/authorize?client_id=465551084711182377&permissions=8&scope=bot`)
         .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
        }
